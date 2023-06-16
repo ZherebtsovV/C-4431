4dfd70849900c12dc271f462e8d41008f5fc59bf
VictorZher~ <Kor23a@mail.ru>
2023-06-16 13:00:28 +0300

    DZ2

---
+++ DZ1_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Hello, World!");
+
+/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA >= 0)
+{
+for (int i = 2; i <= numberA;)
+{
+    Console.Write($"{i}, ");
+    i = i+2;
+}
+}
+else
+{
+for (int i = -2; i >= numberA;)
+{
+    Console.Write($"{i}, ");
+    i = i-2;
+}
+}
---
+++ DZ2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+Console.Write("Введите трехзначное число число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < 100 || numberA > 999)
+{
+Console.WriteLine($"Число {numberA} не трехзначное");
+return;
+}
+Console.WriteLine($"Число {numberA} трехзначное");
+int Second = numberA / 10 % 10;
+Console.WriteLine($"Вторая цифра числа {numberA} - {Second}");
