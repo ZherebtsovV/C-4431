b341813f526faa37fca1ae582644acf03a6dc367
VictorZher~ <Kor23a@mail.ru>
2023-06-29 17:16:28 +0300

    Seminar5

---
+++ DZ4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Hello, World!");
+
+/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int SumNum (int numA)
+{
+    int numB = 0;
+
+    while (numA > 1)
+    {
+      numB = numB + numA % 10;
+      numA = numA / 10;  
+    }
+        return numB;
+}
+
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+System.Console.Write($"Сумма = {SumNum(number)}: ");
