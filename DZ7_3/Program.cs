2410b201f9e45c3eeb560469c1f52622aa06ed95
VictorZher~ <Kor23a@mail.ru>
2023-07-31 15:02:53 +0300

    Add program

---
+++ DZ7_3/Program.cs
@@ -0,0 +1,59 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 10);
+}
+}
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} ");
+}
+System.Console.WriteLine();
+}
+}
+
+void PrintMatrix1(int[,] matrix)
+{
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"Ср. ар. столбца {j} = { sum / matrix.GetLength(0)} ");
+    System.Console.WriteLine();
+}
+}
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+PrintMatrix1(matrix);
